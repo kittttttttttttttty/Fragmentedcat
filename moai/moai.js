4a1a1fe855a9e17ffc1ada653d1097862b998b07
kittttttttttttttty <electrodkitty@gmail.com>
2023-11-13 00:58:52 -0500

    mabu

---
+++ moai/moai.js
@@ -0,0 +1,17 @@
+function clicks() {
+    console.log("clicked")
+    const counter = document.querySelector('#clk'),
+        count = +counter.innerHTML;
+    counter.innerHTML = count + 1;
+}
+function resetss() {
+    console.log("reseted")
+    butto = document.getElementById('clk')
+    butto.innerHTML = '';
+}
+function biggui() {
+    console.log("bigged")
+}
+function smallgui() {
+    console.log("smalled")
+}
