cd716d0619b8460a8cbe267b93323efc5188fab3
kittttttttttttttty <electrodkitty@gmail.com>
2023-11-22 09:35:03 -0500

    hopefully

--- moai/moai.js
+++ moai/moai.js
@@ -1,12 +1,8 @@
 const ul = document.getElementById('leaderboards');
 const list = document.createDocumentFragment();
 
-
-
-
-
 function clicks() {
-    console.log("clicked")
+    console.log("clicked");
     const counter = document.querySelector('#clk'),
         count = +counter.innerHTML;
     if (counter.innerHTML == 'Click') {
@@ -14,39 +10,44 @@ function clicks() {
     } else {
         counter.innerHTML = count + 1;
     }
-
 }
+
 function resetss() {
-    console.log("reseted")
-    butto = document.getElementById('clk')
+    console.log("reseted");
+    const butto = document.getElementById('clk');
     butto.innerHTML = 'Click';
 }
+
 function updatelead() {
-    fetch("https://fragcat-d376.restdb.io/rest/users")
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            let leaderboardd = data;
+    fetch("https://fragcat-d376.restdb.io/rest/users", {
+        headers: {
+            "Accept": "application/json",
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            ul.innerHTML = ''; // Clear the existing leaderboard before updating
+            data.forEach(user => {
+                const li = document.createElement('li');
+                const name = document.createElement('h2');
+                const score = document.createElement('span');
 
-            leaderboardd.map(function(leaderboardd) {
-                let li = document.createElement('li');
-                let name = document.createElement('h2');
-                let score = document.createElement('span');
-
-                name.innerHTML = `${leaderboardd.name}`
-                score.innerHTML = `${leaderboardd.score}`
+                name.innerHTML = `${user.user}`;
+                score.innerHTML = `${user.num}`;
 
                 li.appendChild(name);
                 li.appendChild(score);
                 list.appendChild(li);
             });
+            ul.appendChild(list);
         })
-    ul.appendChild(list);
+        .catch(error => console.error('Error fetching leaderboard:', error));
 }
+
 function smallgui() {
-    console.log("smalled")
+    console.log("smalled");
 }
+
 function backbutton() {
-    location.replace("/")
+    location.replace("/");
 }
