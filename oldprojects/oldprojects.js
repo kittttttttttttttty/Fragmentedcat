0c447ea294c8f7df1c020a1c0e88a49139d30bdc
kittttttttttttttty <electrodkitty@gmail.com>
2023-12-01 09:50:18 -0500

    bu

--- oldprojects/oldprojects.js
+++ oldprojects/oldprojects.js
@@ -1,6 +1,9 @@
 function calcred() {
     location.replace("/oldprojects/calculator")
 }
+function butonred() {
+    location.replace("/oldprojects/button")
+}
 function backbutton() {
     location.replace("/")
 }
